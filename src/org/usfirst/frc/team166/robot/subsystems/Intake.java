dbb12c5ddcfc4459969f54b14ee05b465726bb1b
IanCummings166 <3salamanders@myfairpoint.net>
2016-01-20 19:51:33 -0500

    Added the Skeletons of the Intake
    Added commands and methods to the intake subsystems

--- src/org/usfirst/frc/team166/robot/subsystems/Intake.java
+++ src/org/usfirst/frc/team166/robot/subsystems/Intake.java
@@ -1,18 +1,35 @@
 package org.usfirst.frc.team166.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Intake extends Subsystem {
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
+	DoubleSolenoid Actuator;
+	Victor IntakeSIM;
+
+	public Intake() {
+
+	}
+
+	public void ToggleIntakeMotor() {
+
+	}
+
+	public void ToggleIntakeSolenoid() {
+
+	}
+
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+
+	@Override
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
 }
-
