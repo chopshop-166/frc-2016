c0327dfcd1e68f19f4f6091ed2c55d4dab403fb5
Nick Pittman <astronomer346@gmail.com>
2016-01-22 16:05:09 -0500

    Added proportional control system to the drive function
    Will make sure the robot drives straight when controlled with a joystick

--- src/org/usfirst/frc/team166/robot/RobotMap.java
+++ src/org/usfirst/frc/team166/robot/RobotMap.java
@@ -1,18 +1,21 @@
 package org.usfirst.frc.team166.robot;
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
+
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
+	public static int leftTopDrive = 0;
+	public static int leftBotDrive = 1;
+	public static int rightTopDrive = 2;
+	public static int rightBotDrive = 3;
+	public static int rollerPort = 6;
+
+	public static int leftEncoderA = 8;
+	public static int leftEncoderB = 9;
+	public static int rightEncoderA = 10;
+	public static int rightEncoderB = 11;
+
+	public static int leftJoystickPort = 0;
+	public static int rightJoystickPort = 1;
+	public static int xboxPort = 2;
+
+	public static int gyroPort = 0;
+
 }
