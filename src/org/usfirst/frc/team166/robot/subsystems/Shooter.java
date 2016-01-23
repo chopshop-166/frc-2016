85abe2295a9fcd14e509f7fcf2d0db93a82aa448
Alexa Gardner <beccablu15@gmail.com>
2016-01-23 11:36:04 -0500

    Added Encoder Values in Shooter
    Fixed #7

--- src/org/usfirst/frc/team166/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team166/robot/subsystems/Shooter.java
@@ -23,8 +23,8 @@ public class Shooter extends Subsystem {
 		shooterLeftSide = new Victor(RobotMap.Pwm.LeftShooterMotor);
 		shooterRightSide = new Victor(RobotMap.Pwm.RightShooterMotor);
 		shooterAngle = new Victor(RobotMap.Pwm.ShooterAngleMotor);
-		encoderLeft = new Encoder(null, null);
-		encoderRight = new Encoder(null, null);
+		encoderLeft = new Encoder(12, 13);
+		encoderRight = new Encoder(14, 15);
 	}
 
 	public double getPot() {
