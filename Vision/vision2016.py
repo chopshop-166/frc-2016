581e2b4cecb9ee266cbae24e73ef743dcb8491be
Driver-Station <166.first@gmail.com>
2016-04-01 20:38:25 -0400

    Vision Update
    Updated python code. Contains new HSV thresholds that deal with bright
    likes, such as those at UNH. Also, the target width to shooter angle
    algorithm was slightly modified after open bag day testing.

--- Vision/vision2016.py
+++ Vision/vision2016.py
@@ -1,17 +1,24 @@
 import cv2
 import numpy
 import math
+import time
+from networktables import *
 
-from pynetworktables import *
+NetworkTable.setIPAddress("roborio-166-frc.local")
+NetworkTable.setClientMode()
+NetworkTable.initialize()
 
-NetworkTable.SetIPAddress("10.1.66.2") #Connect to network tables on robot
-NetworkTable.SetClientMode()
-NetworkTable.Initialize()
 
-visionDataTable = NetworkTable.GetTable("visionDataTable") #Connect specifically to vision NetworkTable
+visionDataTable = NetworkTable.getTable("VisionDataTable") #Connect specifically to vision NetworkTable
 
-visionDataTable.PutBoolean("isHot",False) #Define default values for Network Table Variables
-visionDataTable.PutNumber("skinnyOffset",0.0)
+while not visionDataTable.isConnected():
+    time.sleep(.1)
+
+print("We are Connected")
+
+visionDataTable.putNumber("shooterAngle",45.0) #Define default values for Network Table Variables
+visionDataTable.putNumber("xPosition",0.0)
+print("Initialized Values")
 
 def find_distance(x1,y1,x2,y2):
     root = math.sqrt(  ((x2 - x1) ** 2) + ((y2 - y1) ** 2)  )
@@ -25,20 +32,24 @@ def threshold_range(im, lo, hi):
     unused, t2 = cv2.threshold(im, hi, 255, type=cv2.THRESH_BINARY_INV)
     return cv2.bitwise_and(t1, t2)
 def findDistanceToTarget(width):
-    #distance = (-15.26*math.log(width)+82.857)
-    distance = (44.139 * math.exp((-0.012 * width)))
+    #note that the width is multiplied by 2 because of resolution change on the image
+    #this change allows the new resolution to fit with the correct model
+    
+    distance = (44.139 * math.exp((-0.012 * (2 * width))))
     return distance
+
 def findAngle(distance):
-    angle = math.degrees(math.atan(6.5/distance))
+    angle = (.1183*(distance **2 ) - (3.468 * distance) + 69.203)
     return angle;
 
 vc = cv2.VideoCapture()
 
-
-if not vc.open('http://10.1.66.11/mjpg/video.mjpg'): #connect to Axis Camera
+if not vc.open('http://10.1.66.12/mjpg/video.mjpg'): #connect to Axis Camera
 #if not vc.open(0): #connect to Webcam
     print "Could not connect to camera"
     exit(1)
+
+
 while cv2.waitKey(10) <= 0:
 #while i == 0:
     success, img = vc.read()
@@ -46,14 +57,26 @@ while cv2.waitKey(10) <= 0:
         print ("Failure")
         break
 
+
+    cv2.imshow("Source", img)
     #image processing
+
+    scale = 1.0  # whatever scale you want (was .1)
+    img = (img * scale).astype(numpy.uint8)
+
     hsv = cv2.cvtColor(img,cv2.cv.CV_BGR2HSV) # Convert original color image to hsv image
 
     h, s, v = cv2.split(hsv) #Split hsv image into hue/saturation/value images
 
-    h = threshold_range(h, 40, 110) #Isolate only the green in the image
-    s = threshold_range(s, 90, 255)
+   # h = threshold_range(h, 40, 110) #Isolate only the green in the image
+   
+    h = threshold_range(h, 50, 80)
+    s = threshold_range(s, 150, 255)
     v = threshold_range(v, 20, 255)
+
+    #h = threshold_range(h, 55, 69)
+    #s = threshold_range(s, 200, 255)
+    #v = threshold_range(v, 10, 50)
 
     threshold = cv2.bitwise_and(h, cv2.bitwise_and(s, v)) #Combine 3 thresholds into one final threshold image
 
@@ -68,7 +91,7 @@ while cv2.waitKey(10) <= 0:
 
     cv2.drawContours(color, simplecontours, -1, (0,0,255), thickness = 2)
 
-    x =[]
+    x = []
     y = []
     w = []
     h = []
@@ -93,13 +116,16 @@ while cv2.waitKey(10) <= 0:
 
                #Keep in mind, x and y are the actual centers, but xtemp and ytemp is the upper left corner
                
-               if(atemp > 1000):
+               if(atemp > 750):
                    cv2.circle(color,(xtemp + (wtemp/2) ,ytemp + (htemp/2)),2,(0,255,0),thickness = -1)
-                   tempstring = " (%d,%d)" % (xtemp, ytemp)
+                   tempstring = " (%d,%d)" % (x[maxAreaIndex], y[maxAreaIndex])
                    cv2.putText(color, tempstring, (xtemp + wtemp,ytemp + (htemp/2)), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,255), thickness = 1)
                    cv2.putText(color, "%d" %(wtemp), (xtemp + (wtemp/2),ytemp + htemp + 30), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,255), thickness = 1)
                    cv2.putText(color, "%d" %(htemp), (xtemp - 30 ,ytemp + (htemp/2)), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,255), thickness = 1)
-                   cv2.putText(color, "Distance To Target: %d" %(findDistanceToTarget(w[maxAreaIndex])), (0,16), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255),thickness = 1)
-                   cv2.putText(color, "Angle: %d" % (findAngle(findDistanceToTarget(w[maxAreaIndex]))), (0,32), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255),thickness = 1)
-              
+                   cv2.putText(color, "Distance To Target: %.2f" %(findDistanceToTarget((w[maxAreaIndex]))), (0,16), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255),thickness = 1)
+                   cv2.putText(color, "Angle: %d" % (findAngle(findDistanceToTarget((w[maxAreaIndex])))), (0,32), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255),thickness = 1)
+                   visionDataTable.putBoolean("isLargeTargetFound", False)
+                   visionDataTable.putNumber("xPosition", x[maxAreaIndex])
+                   visionDataTable.putNumber("shooterAngle", findAngle(findDistanceToTarget((w[maxAreaIndex]))))
+                   visionDataTable.putNumber("distanceToTarget", findDistanceToTarget(w[maxAreaIndex]))
     cv2.imshow("Cameras are awesome :D", color)
